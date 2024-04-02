d6876ceea34bde5225c21812206960cd62aade8e
Moon0922 <moonnight0216@gmail.com>
2024-04-01 19:52:26 -0700

    Update Program.cs

--- CVApp.Server/Program.cs
+++ CVApp.Server/Program.cs
@@ -32,7 +32,6 @@ builder.Services.AddCors(options => {
     options.AddPolicy("Open", policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.IncludeFields = true; }); ;
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -41,7 +40,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
